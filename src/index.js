eab39b43029ea5d4eb1be238dd1f745ab10b5bea
G0TT0N <gotton92@mail.ru>
2019-08-21 19:15:36 +0300

    add history

--- src/index.js
+++ src/index.js
@@ -34,47 +34,16 @@ let Square = (props) => {
 };
 
 class Board extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null), // заполнить все ячейки массива null
-            xIsNext: true, // изначально первым ходят Х
-        }
-    }
-
-    handleClick(i) {
-        const squares = this.state.squares.slice(); // сохраняем копию массива squares
-        if (calculateWinner(squares) || squares[i]) {
-            return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O'; // если true то Х иначе О
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext, // по клику false
-        }); // перезаписываем стейт новым массивом
-    }
-
     renderSquare(i) {
         return <Square
-            value={this.state.squares[i]} // установить value ячеек значениями из стейта
-            onClick={() => this.handleClick(i)} // переносим функцию
+            value={this.props.squares[i]} // установить value ячеек значениями
+            onClick={() => this.props.onClick(i)} // переносим функцию
         />
-
     }
 
     render() {
-        const winner = calculateWinner(this.state.squares);
-        let status;
-
-        {
-            winner
-                ? status = 'Winner: ' + winner
-                : status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O'); // если true то ходят Х иначе О
-        }
-
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -96,15 +65,73 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null), // заполнить все ячейки массива null
+            }],
+            stepNumber: 0,
+            xIsNext: true, // изначально первым ходят Х
+        }
+    }
+
+    handleClick(i) {
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length - 1];
+        const squares = current.squares.slice(); // сохраняем копию массива squares
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O'; // если true то Х иначе О
+        this.setState({
+            history: history.concat([{ // добавляет еще один массив
+                squares: squares,
+            }]),
+            stepNumber: history.length,
+            xIsNext: !this.state.xIsNext, // по клику false
+        });
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0
+        })
+    }
+
     render() {
+        const history = this.state.history;
+        const current = history[this.state.stepNumber];
+        const winner = calculateWinner(current.squares);
+        const moves = history.map((step, move) => {
+            const desc = move
+                ? 'Run to #' + move
+                : 'Return to start';
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            )
+        });
+        let status;
+
+        winner
+            ? status = 'Winner: ' + winner
+            : status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O'); // если true то ходят Х иначе О
+
+
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board/>
+                    <Board
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)}
+                    />
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
+                    <div> {status} </div>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
