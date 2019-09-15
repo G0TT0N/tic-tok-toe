7f35806c0c7ce7b1113bbd6cb9f520648327f1f0
G0TT0N <gotton92@mail.ru>
2019-09-15 19:35:11 +0300

    Updates

---
+++ precache-manifest.028e88cfc9f87ab7120b9eb8a679d980.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efb1a7ae9b89e9b3b76e6d6dd45608f8",
+    "url": "/tic-tok-toe/index.html"
+  },
+  {
+    "revision": "10fbc010724fcadb7ad7",
+    "url": "/tic-tok-toe/static/css/main.8a935685.chunk.css"
+  },
+  {
+    "revision": "3096f8f1f98d2aee5468",
+    "url": "/tic-tok-toe/static/js/2.fa8eb5ea.chunk.js"
+  },
+  {
+    "revision": "10fbc010724fcadb7ad7",
+    "url": "/tic-tok-toe/static/js/main.70b529b9.chunk.js"
+  },
+  {
+    "revision": "250aed4ed1f543047086",
+    "url": "/tic-tok-toe/static/js/runtime~main.4149c23e.js"
+  }
+]);
